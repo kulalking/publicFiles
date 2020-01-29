ba661f52f184c9ad90bc62af007694debd910412
Sathish Kulal <sathish.kulal@gmail.com>
2020-01-29 15:09:21 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -12,12 +12,8 @@ echo repoName = $repoName
 
 helm3 repo add $repoName $arcPath
 helm3 fetch $repoName/$chart --version $version
-#export releaseName=`helm3 list --all-namespaces --all | grep $chart | cut -f1 -d$'\t'`
-#export namespace=`helm3 list --all-namespaces --all | grep $chart | cut -f2 -d$'\t'`
-
-export releaseName=$(helm3 list --all-namespaces --all | grep $chart | cut -f1 -d$'\t')
-export namespace=$(helm3 list --all-namespaces --all | grep $chart | cut -f2 -d$'\t')
-
+export releaseName=`helm3 list --all-namespaces --all | grep $chart | cut -f1`
+export namespace=`helm3 list --all-namespaces --all | grep $chart | cut -f2`
 
 export filename=$chart-$version.tgz
 helm3 upgrade $releaseName $filename -n $namespace --install --reuse-values
